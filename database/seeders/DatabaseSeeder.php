d0d0302a7ec3eaf8f7863bf749803e2a90ad11a2
Larry Barker <github@larrybarker.me>
2021-02-28 19:31:34 -0600

    add city and observation seeders to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Observation;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CapybaraSeeder::class,
-        ])
+            CitySeeder::class,
+            ObservationSeeder::class,
+        ]);
     }
 }
